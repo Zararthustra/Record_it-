8f31891b0c9f31dde00e84cb3df702f0d85b5b6b
Arthur <arthmayer@outlook.fr>
2021-06-17 19:39:22 +0200

    clean records page with top3 and all scores sorted

--- client/src/pages/Records.js
+++ client/src/pages/Records.js
@@ -1,77 +1,235 @@
-import React from 'react';
-import Axios from 'axios'
+import React, { Component } from 'react'
+import Axios from 'axios';
 import { useState } from "react";
 import { useHistory } from 'react-router-dom';
 import Navigation from '../components/Navigation';
 
 
 
-const Records = () => {
+class Records extends Component {
 
-    //______________________________Variables__________________________________
-    
-    let history = useHistory();
-    const [usersList, setUsersList] = useState([]);
-    let sortDirection = false;
+    //______________________________Constructor__________________________________
+    constructor(props) {
+        super(props);
 
-    //______________________________Functions__________________________________
-    
-    const goHome = () => {
-        history.push('/Home')
-    }
-
-    // Fill the table dynamically with getRecords
-    function loadTableData(tableData) {
-        const tableBody = document.getElementById('tableBody');
-        let dataHtml = "";
-        for (let row of tableData) {
-            const cleanDate = new Date(row.createdAt)
-            dataHtml += `<tr><td>${row.game_name}</td><td>${row.record}</td><td>${row.user_name}</td><td>${cleanDate.toDateString()}</td></tr>`
+        this.state = {
+            topFlappyRecords: [],
+            topSnakeRecords: [],
+            flappyRecords: [],
+            snakeRecords: [],
+            showMe: true
         }
-        tableBody.innerHTML = dataHtml;
-    }
-    
-    const getRecords = async () => {
-        const response = await Axios.get('http://localhost:3001/apiroutes/records')
-            setUsersList(response.data);
-            loadTableData(response.data);
     }
 
-    //_______________________________Return___________________________________
 
-    return (
-        <div className="records">
-            <Navigation />
-            <h1>Records</h1>
-            <button onClick={getRecords}>Load Records</button>
-            <button onClick={goHome}>Back to Home</button>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Game</th>
-                        <th>Record</th>
-                        <th>User</th>
-                        <th>Date</th>
-                    </tr>
-                </thead>
-                <tbody id="tableBody">
-                </tbody>
-            </table>
-            
-        </div>
-    )
+    //________________________________DB Call_________________________________
+
+    async componentDidMount() {
+        //TOP3 records
+        //FLAPPY
+        const topFlappyRecords = await Axios.post('http://localhost:3001/apiroutes/topGameRecords', {
+            game_id: 1 //flappy game_id
+        })
+        this.setState({ topFlappyRecords: topFlappyRecords.data });
+
+        //SNAKE
+        const topSnakeRecords = await Axios.post('http://localhost:3001/apiroutes/topGameRecords', {
+            game_id: 2 //snake game_id
+        })
+        this.setState({ topSnakeRecords: topSnakeRecords.data });
+
+        //All records
+        //FLAPPY
+        const flappyRecords = await Axios.post('http://localhost:3001/apiroutes/gameRecords', {
+            game_id: 1 //flappy game_id
+        })
+        this.setState({ flappyRecords: flappyRecords.data });
+
+        //SNAKE
+        const snakeRecords = await Axios.post('http://localhost:3001/apiroutes/gameRecords', {
+            game_id: 2 //snake game_id
+        })
+        this.setState({ snakeRecords: snakeRecords.data });
+    };
+
+    //_______________________________Render___________________________________
+
+    render() {
+
+        //______________________________Variables__________________________________
+        // TOP FLAPPY
+        const topFlappyRecords = this.state.topFlappyRecords.map(record => <div>{record.record}</div>)
+        const topFlappyUsers = this.state.topFlappyRecords.map(record => <div>{record.user_name}</div>)
+        const topFlappyDate = this.state.topFlappyRecords.map(record => {
+            const cleanDate = new Date(record.createdAt)
+            return <div>{cleanDate.toDateString()}</div>
+        })
+        // ALL FLAPPY
+        const flappyRecords = this.state.flappyRecords.map(record => <div>{record.record}</div>)
+        const flappyUsers = this.state.flappyRecords.map(record => <div>{record.user_name}</div>)
+        const flappyDate = this.state.flappyRecords.map(record => {
+            const cleanDate = new Date(record.createdAt)
+            return <div>{cleanDate.toDateString()}</div>
+        })
+
+        // TOP SNAKE
+        const topSnakeRecords = this.state.topSnakeRecords.map(record => <div>{record.record}</div>)
+        const topSnakeUsers = this.state.topSnakeRecords.map(record => <div>{record.user_name}</div>)
+        const topSnakeDate = this.state.topSnakeRecords.map(record => {
+            const cleanDate = new Date(record.createdAt)
+            return <div>{cleanDate.toDateString()}</div>
+        })
+        // ALL SNAKE
+        const snakeRecords = this.state.snakeRecords.map(record => <div>{record.record}</div>)
+        const snakeUsers = this.state.snakeRecords.map(record => <div>{record.user_name}</div>)
+        const snakeDate = this.state.snakeRecords.map(record => {
+            const cleanDate = new Date(record.createdAt)
+            return <div>{cleanDate.toDateString()}</div>
+        })
+
+        const switchState = () => {
+            if (this.state.showMe === true) {
+
+                this.setState({ showMe: false });
+            } else {
+                this.setState({ showMe: true });
+            }
+        }
+
+        if (this.state.showMe === true) {
+            return (
+                <div className="allrecords">
+                    <Navigation />
+                    <button onClick={switchState}>All records</button>
+                    <h1>TOP 3</h1>
+                    <div className="allgametables">
+                        <div className="gametable">
+                            <header><h2>Flappy Holbie</h2></header>
+                            <table className="recordtable">
+                                <thead>
+                                    <tr>
+                                        <th>Record</th>
+                                        <th>User</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            {topFlappyRecords}
+                                        </td>
+                                        <td>
+                                            {topFlappyUsers}
+                                        </td>
+                                        <td>
+                                            {topFlappyDate}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div className="gametable">
+                            <header><h2>Snake</h2></header>
+                            <table className="recordtable">
+                                <thead>
+                                    <tr>
+                                        <th>Record</th>
+                                        <th>User</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            {topSnakeRecords}
+                                        </td>
+                                        <td>
+                                            {topSnakeUsers}
+                                        </td>
+                                        <td>
+                                            {topSnakeDate}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        else if (this.state.showMe === false) {
+            return (
+                <>
+                    <Navigation />
+                    <div className="allrecords">
+                        <button onClick={switchState}>Top 3 records</button>
+                        <h1>All records</h1>
+                        <div className="allgametables">
+                            <div className="gametable">
+                                <header><h2>Flappy Holbie</h2></header>
+                                <table className="recordtable">
+                                    <thead>
+                                        <tr>
+                                            <th>Record</th>
+                                            <th>User</th>
+                                            <th>Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                {flappyRecords}
+                                            </td>
+                                            <td>
+                                                {flappyUsers}
+                                            </td>
+                                            <td>
+                                                {flappyDate}
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div className="gametable">
+                                <header><h2>Snake</h2></header>
+                                <table className="recordtable">
+                                    <thead>
+                                        <tr>
+                                            <th className="hey">Record</th>
+                                            <th>User</th>
+                                            <th>Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                {snakeRecords}
+                                            </td>
+                                            <td>
+                                                {snakeUsers}
+                                            </td>
+                                            <td>
+                                                {snakeDate}
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )
+        }
+
+        //_______________________________Return___________________________________
+        return (
+            <div className="records">
+                <h1>Records</h1>
+            </div>
+        )
+    }
 }
 
 export default Records;
-
-//{usersList.map((value) => {
-//    console.log(usersList);
-//    return <div className="record">
-//        <h2>id:</h2>
-//        <p>{value.id}</p>
-//        <h2>Name:</h2>
-//        <p>{value.name}</p>
-//        <h2>Password:</h2>
-//        <p>{value.password}</p>
-//    </div>
-//})}
