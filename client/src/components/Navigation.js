ff0cc222e74a98da21e1a31d5e392045a37871bc
Arthur <arthmayer@outlook.fr>
2021-06-18 02:33:22 +0200

    add a beautiful sidebar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -4,41 +4,79 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <div className="menu">
-      <ul className="Navigation">
-        <li id="login">
-            <NavLink exact to="/" activeClassName="navActive">
-              <span>Login</span>
-            </NavLink>
-        </li>
+    <>
+      <body>
+        <nav class="navbar">
+          <ul class="navbar-nav">
 
-        <li id="signup">
-          <NavLink exact to="/SignUp" activeClassName="navActive">
-            <span>SignUp</span>
-          </NavLink>
-        </li>
-        <li id="home">
-          <NavLink exact to="/Home" activeClassName="navActive">
-            <span>Home</span>
-          </NavLink>
-        </li>
-        <li id="games">
-          <NavLink exact to="/Games" activeClassName="navActive">
-            <span>Games</span>
-          </NavLink>
-        </li>
-        <li id="records">
-          <NavLink exact to="/Records" activeClassName="navActive">
-            <span>Records</span>
-          </NavLink>
-        </li>
-        <li id="profile">
-          <NavLink exact to="/Profile" activeClassName="navActive">
-            <span>Profile</span>
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+            <li class="logo">
+              <a href="#" class="nav-link">
+                <span class="link-text logo-text">Record !t</span>
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fad"
+                  data-icon="angle-double-right"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                  class="svg-inline--fa fa-angle-double-right fa-w-14 fa-5x"
+                >
+                  <g class="fa-group">
+                    <path
+                      fill="currentColor"
+                      d="M224 273L88.37 409a23.78 23.78 0 0 1-33.8 0L32 386.36a23.94 23.94 0 0 1 0-33.89l96.13-96.37L32 159.73a23.94 23.94 0 0 1 0-33.89l22.44-22.79a23.78 23.78 0 0 1 33.8 0L223.88 239a23.94 23.94 0 0 1 .1 34z"
+                      class="fa-secondary"
+                    ></path>
+                    <path
+                      fill="currentColor"
+                      d="M415.89 273L280.34 409a23.77 23.77 0 0 1-33.79 0L224 386.26a23.94 23.94 0 0 1 0-33.89L320.11 256l-96-96.47a23.94 23.94 0 0 1 0-33.89l22.52-22.59a23.77 23.77 0 0 1 33.79 0L416 239a24 24 0 0 1-.11 34z"
+                      class="fa-primary"
+                    ></path>
+                  </g>
+                </svg>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="/Home" class="nav-link">
+                  <span class="link-text">Home</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="/Games" class="nav-link">
+                  <span class="link-text">Games</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="/Records" class="nav-link">
+                <span class="link-text">Records</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="/Profile" class="nav-link">
+                <span class="link-text">Profile</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="/" class="nav-link">
+                <span class="link-text">Login</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="/Signup" class="nav-link">
+                <span class="link-text">Sign Up</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </body>
+    </>
   );
 };
 
